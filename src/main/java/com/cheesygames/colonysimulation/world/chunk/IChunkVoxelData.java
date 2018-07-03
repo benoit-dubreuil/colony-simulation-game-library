48530e6ea0b4544d1def06e1a4f69af6db27aa25
Benoît Dubreuil <b.dubreuil@hotmail.com>
2018-07-03 16:53:13 -0400

    Added javadoc

--- src/main/java/com/cheesygames/colonysimulation/world/chunk/IChunkVoxelData.java
+++ src/main/java/com/cheesygames/colonysimulation/world/chunk/IChunkVoxelData.java
@@ -12,5 +12,15 @@ public interface IChunkVoxelData {
         return getVoxelAt(index.x, index.y, index.z);
     }
 
-    VoxelType getSideVoxelAt(Direction3D direction, int x, int y);
+    /**
+     * Gets a voxel from the side specified by its direction. The direction defines the origin of the coordinate system.
+     *
+     * @param direction The direction that defines the side. Orthogonal directions are accepted.
+     * @param x     The X coordinate of the voxel to get. If the direction affects this coordinate's axis, then it is an offset from the direction's side.
+     * @param y     The Y coordinate of the voxel to get. If the direction affects this coordinate's axis, then it is an offset from the direction's side.
+     * @param z     The Z coordinate of the voxel to get. If the direction affects this coordinate's axis, then it is an offset from the direction's side.
+     *
+     * @return The voxel at the specified side of the chunk at the given coordinates, whilst keeping in mind that the direction defines the origin of the coordinate system.
+     */
+    VoxelType getVoxelFromSide(Direction3D direction, int x, int y, int z);
 }
