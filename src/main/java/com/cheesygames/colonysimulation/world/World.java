93da7d97a851c8d5cd5c0e15397a35bcda9abecc
Benoît Dubreuil <b.dubreuil@hotmail.com>
2018-07-05 15:17:04 -0400

    Added getAdjacentChunks method

--- src/main/java/com/cheesygames/colonysimulation/world/World.java
+++ src/main/java/com/cheesygames/colonysimulation/world/World.java
@@ -1,7 +1,10 @@
 package com.cheesygames.colonysimulation.world;
 
+import com.cheesygames.colonysimulation.math.direction.Direction3D;
 import com.cheesygames.colonysimulation.math.vector.Vector3i;
 import com.cheesygames.colonysimulation.world.chunk.Chunk;
+import com.cheesygames.colonysimulation.world.chunk.EmptyChunk;
+import com.cheesygames.colonysimulation.world.chunk.IChunkVoxelData;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,5 +19,18 @@ public class World {
 
     public Chunk getChunkAt(Vector3i index) {
         return m_chunks.get(index);
+    }
+
+    public Map<Direction3D, IChunkVoxelData> getAdjacentChunks(Vector3i index) {
+        Map<Direction3D, IChunkVoxelData> adjacentChunks = new HashMap<>(Direction3D.ORTHOGONALS.length);
+        Vector3i adjacentChunkIndex = new Vector3i();
+
+        for (Direction3D direction : Direction3D.ORTHOGONALS) {
+            adjacentChunkIndex.set(index);
+            IChunkVoxelData chunk = m_chunks.get(adjacentChunkIndex.addLocal(direction.getDirection()));
+            adjacentChunks.put(direction, chunk != null ? chunk : EmptyChunk.DEFAULT_EMPTY_CHUNK);
+        }
+
+        return adjacentChunks;
     }
 }
