9a79673598af02dcc97c2f6169ce826990ebff33
Benoît Dubreuil <b.dubreuil@hotmail.com>
2018-07-05 15:31:04 -0400

    Added more logic to the generateMesh method

--- src/main/java/com/cheesygames/colonysimulation/world/chunk/MeshGenerator.java
+++ src/main/java/com/cheesygames/colonysimulation/world/chunk/MeshGenerator.java
@@ -1,9 +1,12 @@
 package com.cheesygames.colonysimulation.world.chunk;
 
+import com.cheesygames.colonysimulation.math.direction.Direction3D;
 import com.cheesygames.colonysimulation.math.vector.Vector3i;
 import com.cheesygames.colonysimulation.world.World;
 import com.cheesygames.colonysimulation.world.chunk.voxel.VoxelType;
 import com.jme3.scene.Mesh;
+
+import java.util.Map;
 
 public class MeshGenerator {
 
@@ -17,14 +20,32 @@ public class MeshGenerator {
         // TODO
 
         m_tmpIndex.set(index);
+        Mesh mesh = new Mesh();
+
         Chunk chunk = world.getChunkAt(index);
         Vector3i chunkSize = chunk.getSize();
-        Mesh mesh = new Mesh();
+
+        Map<Direction3D, IChunkVoxelData> adjacentChunks = world.getAdjacentChunks(index);
 
         for (int x = 0; x < chunkSize.x; ++x) {
             for (int y = 0; y < chunkSize.y; ++y) {
                 for (int z = 0; z < chunkSize.z; ++z) {
-           //         m_voxels[x][y][z] = VoxelType.SOLID;
+                    for (Direction3D cubeFace : Direction3D.ORTHOGONALS) {
+                        VoxelType adjacentVoxelType;
+                        int adjacentVoxelX = x + cubeFace.getDirectionX();
+                        int adjacentVoxelY = y + cubeFace.getDirectionY();
+                        int adjacentVoxelZ = z + cubeFace.getDirectionZ();
+
+                        if (adjacentVoxelX <= 0 || adjacentVoxelX >= chunkSize.x || adjacentVoxelY <= 0 || adjacentVoxelY >= chunkSize.y || adjacentVoxelZ <= 0
+                            || adjacentVoxelZ >= chunkSize.z) {
+                            adjacentVoxelType = adjacentChunks.get(cubeFace.getDirection()).getVoxelFromSide(cubeFace.getOpposite(), x, y, z);
+                        }
+                        else {
+                            adjacentVoxelType = chunk.getVoxelAt(adjacentVoxelX, adjacentVoxelY, adjacentVoxelZ);
+                        }
+
+
+                    }
                 }
             }
         }
