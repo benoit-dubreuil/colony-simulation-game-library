2a0928256e1d8989fcc41c160421c568bb6798f2
Benoît Dubreuil <b.dubreuil@hotmail.com>
2018-07-10 15:17:51 -0400

    Added utility methods to get the absolute index of a voxel

--- src/main/java/com/cheesygames/colonysimulation/world/World.java
+++ src/main/java/com/cheesygames/colonysimulation/world/World.java
@@ -11,17 +11,20 @@ import java.util.Map;
 
 public class World {
 
-    private static final Vector3i DEFAULT_CHUNK_SIZE = new Vector3i(32, 32, 32);
+    private static final Vector3i DEFAULT_CHUNK_SIZE_BITS = new Vector3i(5, 5, 5);
+    private static final Vector3i DEFAULT_CHUNK_SIZE = new Vector3i(2 << DEFAULT_CHUNK_SIZE_BITS.x, 2 << DEFAULT_CHUNK_SIZE_BITS.y, 2 << DEFAULT_CHUNK_SIZE_BITS.z);
 
     private Map<Vector3i, Chunk> m_chunks;
     private IMeshGenerator m_meshGenerator;
     private IWorldGenerator m_worldGenerator;
+    private Vector3i m_chunkSizeBits;
     private Vector3i m_chunkSize;
 
     public World() {
         this.m_chunks = new HashMap<>();
         this.m_meshGenerator = new BlockMeshGenerator();
-        this.m_worldGenerator = new GradientWorldGenerator(new Vector3f(16, 16, 16));
+        this.m_worldGenerator = new GradientWorldGenerator(new Vector3f(-16, 16, -16));
+        this.m_chunkSizeBits = new Vector3i(DEFAULT_CHUNK_SIZE_BITS);
         this.m_chunkSize = new Vector3i(DEFAULT_CHUNK_SIZE);
 
         assert MathExt.isPowerOfTwo(m_chunkSize.getX());
@@ -43,6 +46,55 @@ public class World {
         return m_chunks;
     }
 
+    /**
+     * Gets the absolute index of a chunk relative position according to its chunk's index. In other words, it gives an integer world position.
+     *
+     * @param chunkIndex         The chunk's index.
+     * @param chunkRelativeIndex The chunk relative index, a.k.a. the index in the chunk's data multidimensional array.
+     * @param chunkSizeBits      The bit shift count for the chunk size. This allows to not mention the axis.
+     *
+     * @retur The absolute index.
+     */
+    private int getAbsoluteIndex(int chunkIndex, int chunkRelativeIndex, int chunkSizeBits) {
+        return chunkIndex << chunkSizeBits + chunkRelativeIndex;
+    }
+
+    /**
+     * Gets the absolute index of a chunk relative position according to its chunk's index and the specified axis. In other words, it gives an integer world position on an axis.
+     *
+     * @param chunkIndex         The chunk's index for the specified axis.
+     * @param chunkRelativeIndex The chunk relative index, a.k.a. the axis index in the chunk's data multidimensional array.
+     *
+     * @retur The absolute index for the specified axis.
+     */
+    public int getAbsoluteIndexX(int chunkIndex, int chunkRelativeIndex) {
+        return getAbsoluteIndex(chunkIndex, chunkRelativeIndex, m_chunkSizeBits.x);
+    }
+
+    /**
+     * Gets the absolute index of a chunk relative position according to its chunk's index and the specified axis. In other words, it gives an integer world position on an axis.
+     *
+     * @param chunkIndex         The chunk's index for the specified axis.
+     * @param chunkRelativeIndex The chunk relative index, a.k.a. the axis index in the chunk's data multidimensional array.
+     *
+     * @retur The absolute index for the specified axis.
+     */
+    public int getAbsoluteIndexY(int chunkIndex, int chunkRelativeIndex) {
+        return getAbsoluteIndex(chunkIndex, chunkRelativeIndex, m_chunkSizeBits.y);
+    }
+
+    /**
+     * Gets the absolute index of a chunk relative position according to its chunk's index and the specified axis. In other words, it gives an integer world position on an axis.
+     *
+     * @param chunkIndex         The chunk's index for the specified axis.
+     * @param chunkRelativeIndex The chunk relative index, a.k.a. the axis index in the chunk's data multidimensional array.
+     *
+     * @retur The absolute index for the specified axis.
+     */
+    public int getAbsoluteIndexZ(int chunkIndex, int chunkRelativeIndex) {
+        return getAbsoluteIndex(chunkIndex, chunkRelativeIndex, m_chunkSizeBits.z);
+    }
+
     public Chunk getChunkAt(Vector3i index) {
         return m_chunks.get(index);
     }
@@ -58,6 +110,10 @@ public class World {
         }
 
         return adjacentChunks;
+    }
+
+    public Vector3i getChunkSizeBits() {
+        return m_chunkSizeBits;
     }
 
     public Vector3i getChunkSize() {
