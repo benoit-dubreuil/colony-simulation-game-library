3e5d544a194201ad160f5e936ff92474531fb978
Benoit Dubreuil <lol12lol12lol12lol12@gmail.com>
2018-07-10 21:36:55 -0400

    Added the computeCenter method

--- src/main/java/com/cheesygames/colonysimulation/world/chunk/IChunk.java
+++ src/main/java/com/cheesygames/colonysimulation/world/chunk/IChunk.java
@@ -1,14 +1,27 @@
 package com.cheesygames.colonysimulation.world.chunk;
 
+import com.cheesygames.colonysimulation.GameGlobal;
 import com.cheesygames.colonysimulation.math.MathExt;
 import com.cheesygames.colonysimulation.math.direction.Direction3D;
 import com.cheesygames.colonysimulation.math.vector.Vector3i;
 import com.cheesygames.colonysimulation.world.chunk.voxel.VoxelType;
+import com.jme3.math.Vector3f;
 
 /**
  * Interface for a world chunk. A chunk supposedly holds a 3D array of voxel data and has an index.
  */
 public interface IChunk extends IChunkVoxelData {
+
+    /**
+     * Computes a new {@link Vector3f} that is the absolute center of the chunk.
+     *
+     * @return A newly created {@link Vector3f} that is the absolute center of the chunk.
+     */
+    default Vector3f computeCenter() {
+        return new Vector3f(GameGlobal.world.getAbsoluteIndexX(getIndex().x, 0) + getSize().x / 2f + 0.5f,
+            GameGlobal.world.getAbsoluteIndexY(getIndex().y, 0) + getSize().y / 2f + 0.5f,
+            GameGlobal.world.getAbsoluteIndexZ(getIndex().z, 0) + getSize().z / 2f + 0.5f);
+    }
 
     @Override
     default VoxelType getVoxelFromSide(Direction3D direction, int x, int y, int z) {
