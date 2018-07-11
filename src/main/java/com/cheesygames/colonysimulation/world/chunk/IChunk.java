c91dcc9b658e7b051ed537059f5d20f081a00881
Benoît Dubreuil <b.dubreuil@hotmail.com>
2018-07-11 11:47:04 -0400

    Fixed the method getVoxelFromPositiveSide

--- src/main/java/com/cheesygames/colonysimulation/world/chunk/IChunk.java
+++ src/main/java/com/cheesygames/colonysimulation/world/chunk/IChunk.java
@@ -1,7 +1,6 @@
 package com.cheesygames.colonysimulation.world.chunk;
 
 import com.cheesygames.colonysimulation.GameGlobal;
-import com.cheesygames.colonysimulation.math.MathExt;
 import com.cheesygames.colonysimulation.math.direction.Direction3D;
 import com.cheesygames.colonysimulation.math.vector.Vector3i;
 import com.cheesygames.colonysimulation.world.chunk.voxel.VoxelType;
@@ -35,10 +34,10 @@ public interface IChunk extends IChunkVoxelData {
     }
 
     @Override
-    default VoxelType getVoxelFromSide(Direction3D direction, int x, int y, int z) {
-        return getVoxelAt((1 - MathExt.indexifyNormalZeroPositive(direction.getDirectionX())) * (getSize().x - 1) + ((direction.getDirectionX() | 1) * x),
-            (1 - MathExt.indexifyNormalZeroPositive(direction.getDirectionY())) * (getSize().y - 1) + ((direction.getDirectionY() | 1) * y),
-            (1 - MathExt.indexifyNormalZeroPositive(direction.getDirectionZ())) * (getSize().z - 1) + ((direction.getDirectionZ() | 1) * z));
+    default VoxelType getVoxelFromPositiveSide(Direction3D direction, int x, int y, int z) {
+        return getVoxelAt(direction.getDirectionX() * direction.getDirectionX() * (getSize().x - 1) + ((-(direction.getDirectionX() * direction.getDirectionX()) | 1) * x),
+            direction.getDirectionY() * direction.getDirectionY() * (getSize().y - 1) + ((-(direction.getDirectionY() * direction.getDirectionY()) | 1) * y),
+            direction.getDirectionZ() * direction.getDirectionZ() * (getSize().z - 1) + ((-(direction.getDirectionZ() * direction.getDirectionZ()) | 1) * z));
     }
 
     Vector3i getIndex();
