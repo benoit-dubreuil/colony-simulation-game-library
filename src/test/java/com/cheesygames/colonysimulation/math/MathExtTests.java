85e578c1f17a96a825250d211a598c7497dfe2ca
Benoît Dubreuil <b.dubreuil@hotmail.com>
2018-07-25 14:15:18 -0400

    Added the tests for the method abs(float)

--- src/test/java/com/cheesygames/colonysimulation/math/MathExtTests.java
+++ src/test/java/com/cheesygames/colonysimulation/math/MathExtTests.java
@@ -2,6 +2,9 @@ package com.cheesygames.colonysimulation.math;
 
 import org.junit.jupiter.api.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -40,18 +43,97 @@ public class MathExtTests {
     }
 
     @Test
-    public void abs_intMaxInt_positive() {
+    public void abs_intMax_positive() {
         assertEquals(Integer.MAX_VALUE, MathExt.abs(Integer.MAX_VALUE));
         assertTrue(MathExt.abs(Integer.MAX_VALUE) >= 0);
     }
+
 
     /**
      * The minimum integer does not have positive equivalent integer because the number zero counts as a positive number and thus the maximum integer is the absolute negative
      * number + 1.
      */
     @Test
-    public void abs_intMinInt_negative() {
+    public void abs_intMin_negative() {
         assertEquals(Integer.MIN_VALUE, MathExt.abs(Integer.MIN_VALUE));
         assertTrue(MathExt.abs(Integer.MIN_VALUE) < 0);
+    }
+
+    @TestFactory
+    public Collection<DynamicTest> abs_floatZeroPositive_positive() {
+        List<DynamicTest> tests = new ArrayList<>();
+
+        tests.add(DynamicTest.dynamicTest("test_0", () -> {
+            assertEquals(0f, MathExt.abs(0f));
+            assertTrue(MathExt.abs(0f) == 0f);
+        }));
+
+        for (int i = 1; i < 10; ++i) {
+            float number = m_random.nextFloat() * Float.MAX_VALUE;
+
+            tests.add(DynamicTest.dynamicTest("test_" + i, () -> {
+                assertEquals(number, MathExt.abs(number));
+                assertTrue(MathExt.abs(number) == number);
+            }));
+        }
+
+        return tests;
+    }
+
+    @TestFactory
+    public Collection<DynamicTest> abs_floatNegative_positive() {
+        List<DynamicTest> tests = new ArrayList<>();
+
+        for (int i = 0; i < 10; ++i) {
+            float number = -m_random.nextFloat() * Float.MAX_VALUE;
+
+            tests.add(DynamicTest.dynamicTest("test_" + i, () -> {
+                assertEquals(-number, MathExt.abs(number));
+                assertTrue(MathExt.abs(number) == -number);
+            }));
+        }
+
+        return tests;
+    }
+
+    @Test
+    public void abs_floatPositiveInfinity_positive() {
+        assertEquals(Float.POSITIVE_INFINITY, MathExt.abs(Float.POSITIVE_INFINITY));
+        assertTrue(MathExt.abs(Float.POSITIVE_INFINITY) >= 0);
+    }
+
+    @Test
+    public void abs_floatNegativeInfinity_positive() {
+        assertEquals(-Float.NEGATIVE_INFINITY, MathExt.abs(Float.NEGATIVE_INFINITY));
+        assertTrue(MathExt.abs(Float.NEGATIVE_INFINITY) >= 0);
+    }
+
+    @Test
+    public void abs_floatNaN_equals() {
+        assertEquals(Float.NaN, MathExt.abs(Float.NaN));
+    }
+
+    @Test
+    public void abs_floatMaxPositive_positive() {
+        assertEquals(Float.MAX_VALUE, MathExt.abs(Float.MAX_VALUE));
+        assertTrue(MathExt.abs(Float.MAX_VALUE) >= 0);
+    }
+
+    @Test
+    public void abs_floatMinPositive_positive() {
+        assertEquals(Float.MIN_VALUE, MathExt.abs(Float.MIN_VALUE));
+        assertTrue(MathExt.abs(Float.MIN_VALUE) >= 0);
+    }
+
+    @Test
+    public void abs_floatMaxNegative_positive() {
+        assertEquals(Float.MAX_VALUE, MathExt.abs(-Float.MAX_VALUE));
+        assertTrue(MathExt.abs(-Float.MAX_VALUE) >= 0);
+    }
+
+    @Test
+    public void abs_floatMinNegative_positive() {
+        assertEquals(Float.MIN_VALUE, MathExt.abs(-Float.MIN_VALUE));
+        assertTrue(MathExt.abs(-Float.MIN_VALUE) >= 0);
     }
 }
