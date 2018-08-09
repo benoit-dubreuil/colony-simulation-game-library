13dacffde7de3ada9cf3883123308b0eb596782a
Benoît Dubreuil <b.dubreuil@hotmail.com>
2018-08-09 15:44:10 -0400

    Added the getters and setters for the light intensity

--- src/main/java/com/cheesygames/colonysimulation/world/chunk/voxel/VoxelLightUtils.java
+++ src/main/java/com/cheesygames/colonysimulation/world/chunk/voxel/VoxelLightUtils.java
@@ -5,12 +5,18 @@ package com.cheesygames.colonysimulation.world.chunk.voxel;
  */
 public final class VoxelLightUtils {
 
-    private static final int INDIVIDUAL_LIGHT_BIT_COUNT = 4;
+    public static final int INDIVIDUAL_LIGHT_BIT_COUNT = 4;
+    public static final int LIGHT_MAXIMUM_INTENSITY = 0xF;
 
-    private static final int R_LIGHT_BITS = 0xF;
-    private static final int G_LIGHT_BITS = R_LIGHT_BITS << INDIVIDUAL_LIGHT_BIT_COUNT;
-    private static final int B_LIGHT_BITS = G_LIGHT_BITS << INDIVIDUAL_LIGHT_BIT_COUNT;
-    private static final int SUN_LIGHT_BITS = B_LIGHT_BITS << INDIVIDUAL_LIGHT_BIT_COUNT;
+    public static final int R_LIGHT_BIT_POSITION = 0;
+    public static final int G_LIGHT_BIT_POSITION = R_LIGHT_BIT_POSITION + INDIVIDUAL_LIGHT_BIT_COUNT;
+    public static final int B_LIGHT_BIT_POSITION = G_LIGHT_BIT_POSITION + INDIVIDUAL_LIGHT_BIT_COUNT;
+    public static final int SUN_LIGHT_BIT_POSITION = B_LIGHT_BIT_POSITION + INDIVIDUAL_LIGHT_BIT_COUNT;
+
+    public static final int R_LIGHT_BITS = 0xF << R_LIGHT_BIT_POSITION;
+    public static final int G_LIGHT_BITS = 0xF << G_LIGHT_BIT_POSITION;
+    public static final int B_LIGHT_BITS = 0xF << B_LIGHT_BIT_POSITION;
+    public static final int SUN_LIGHT_BITS = 0xF << SUN_LIGHT_BIT_POSITION;
 
     /**
      * Gets the red component of the supplied light.
@@ -98,6 +104,102 @@ public final class VoxelLightUtils {
      */
     public static int setSun(int light, int sun) {
         return light | getSun(sun);
+    }
+
+    /**
+     * Gets the red component's intensity of the supplied light. The minimum intensity is 0. The maximum intensity is defined by 2 power of {@link #INDIVIDUAL_LIGHT_BIT_COUNT}.
+     *
+     * @param light The light that needs its red component's intensity returned.
+     *
+     * @return The red component's intensity of the supplied light.
+     */
+    public static int getRedIntensity(int light) {
+        return getRed(light) >>> R_LIGHT_BIT_POSITION;
+    }
+
+    /**
+     * Gets the green component's intensity of the supplied light. The minimum intensity is 0. The maximum intensity is defined by 2 power of {@link #INDIVIDUAL_LIGHT_BIT_COUNT}.
+     *
+     * @param light The light that needs its green component's intensity returned.
+     *
+     * @return The green component's intensity of the supplied light.
+     */
+    public static int getGreenIntensity(int light) {
+        return getGreen(light) >>> R_LIGHT_BIT_POSITION;
+    }
+
+    /**
+     * Gets the blue component's intensity of the supplied light. The minimum intensity is 0. The maximum intensity is defined by 2 power of {@link #INDIVIDUAL_LIGHT_BIT_COUNT}.
+     *
+     * @param light The light that needs its blue component's intensity returned.
+     *
+     * @return The blue component's intensity of the supplied light.
+     */
+    public static int getBlueIntensity(int light) {
+        return getBlue(light) >>> R_LIGHT_BIT_POSITION;
+    }
+
+    /**
+     * Gets the sun component's intensity of the supplied light. The minimum intensity is 0. The maximum intensity is defined by 2 power of {@link #INDIVIDUAL_LIGHT_BIT_COUNT}.
+     *
+     * @param light The light that needs its sun component's intensity returned.
+     *
+     * @return The sun component's intensity of the supplied light.
+     */
+    public static int getSunIntensity(int light) {
+        return getSun(light) >>> R_LIGHT_BIT_POSITION;
+    }
+
+    /**
+     * Sets the red component's intensity of the supplied. The intensity must be inclusively between 0 and {@link #LIGHT_MAXIMUM_INTENSITY}. In other words, the intensity of a
+     * light component is the value of the latter without the bit shift.
+     *
+     * @param light        The light that needs its red component's intensity set.
+     * @param redIntensity The red component's intensity to set to the supplied light.
+     *
+     * @return The light which its red component's intensity was set.
+     */
+    public static int setRedIntensity(int light, int redIntensity) {
+        return light | ((redIntensity << R_LIGHT_BIT_POSITION) & R_LIGHT_BITS);
+    }
+
+    /**
+     * Sets the green component's intensity of the supplied. The intensity must be inclusively between 0 and {@link #LIGHT_MAXIMUM_INTENSITY}. In other words, the intensity of a
+     * light component is the value of the latter without the bit shift.
+     *
+     * @param light        The light that needs its green component's intensity set.
+     * @param greenIntensity The green component's intensity to set to the supplied light.
+     *
+     * @return The light which its green component's intensity was set.
+     */
+    public static int setGreenIntensity(int light, int greenIntensity) {
+        return light | ((greenIntensity << G_LIGHT_BIT_POSITION) & G_LIGHT_BITS);
+    }
+
+    /**
+     * Sets the blue component's intensity of the supplied. The intensity must be inclusively between 0 and {@link #LIGHT_MAXIMUM_INTENSITY}. In other words, the intensity of a
+     * light component is the value of the latter without the bit shift.
+     *
+     * @param light        The light that needs its blue component's intensity set.
+     * @param blueIntensity The blue component's intensity to set to the supplied light.
+     *
+     * @return The light which its blue component's intensity was set.
+     */
+    public static int setBlueIntensity(int light, int blueIntensity) {
+        return light | ((blueIntensity << B_LIGHT_BIT_POSITION) & B_LIGHT_BITS);
+    }
+
+    /**
+     * Sets the sun component's intensity of the supplied. The intensity must be inclusively between 0 and {@link #LIGHT_MAXIMUM_INTENSITY}. In other words, the intensity of a
+     * light component is the value of the latter without the bit shift.
+     *
+     * @param light        The light that needs its sun component's intensity set.
+     * @param sunIntensity The sun component's intensity to set to the supplied light.
+     *
+     * @return The light which its sun component's intensity was set.
+     */
+    public static int setSunIntensity(int light, int sunIntensity) {
+        return light | ((sunIntensity << SUN_LIGHT_BIT_POSITION) & SUN_LIGHT_BITS);
     }
 
     private VoxelLightUtils() {
