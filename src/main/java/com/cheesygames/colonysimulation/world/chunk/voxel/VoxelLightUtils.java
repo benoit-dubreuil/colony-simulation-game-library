ecb130af045af516d16d24d51b385b23b1e05389
Benoît Dubreuil <b.dubreuil@hotmail.com>
2018-08-14 16:09:34 -0400

    Added the methods getComponent and getComponentIntensity. Bug fix : The get*Intensity methods were not using the right constants.

--- src/main/java/com/cheesygames/colonysimulation/world/chunk/voxel/VoxelLightUtils.java
+++ src/main/java/com/cheesygames/colonysimulation/world/chunk/voxel/VoxelLightUtils.java
@@ -5,18 +5,39 @@ package com.cheesygames.colonysimulation.world.chunk.voxel;
  */
 public final class VoxelLightUtils {
 
+    // Must be power of 2
     public static final int INDIVIDUAL_LIGHT_BIT_COUNT = 4;
     public static final int LIGHT_MAXIMUM_INTENSITY = 0xF;
 
-    public static final int R_LIGHT_BIT_POSITION = 0;
-    public static final int G_LIGHT_BIT_POSITION = R_LIGHT_BIT_POSITION + INDIVIDUAL_LIGHT_BIT_COUNT;
-    public static final int B_LIGHT_BIT_POSITION = G_LIGHT_BIT_POSITION + INDIVIDUAL_LIGHT_BIT_COUNT;
-    public static final int SUN_LIGHT_BIT_POSITION = B_LIGHT_BIT_POSITION + INDIVIDUAL_LIGHT_BIT_COUNT;
+    public static final int R_INDEX = 0;
+    public static final int G_INDEX = R_INDEX + 1;
+    public static final int B_INDEX = G_INDEX + 1;
+    public static final int SUN_INDEX = B_INDEX + 1;
+    public static final int COLOR_COMPONENT_COUNT = SUN_INDEX + 1;
+
+    public static final int R_LIGHT_BIT_POSITION = R_INDEX * INDIVIDUAL_LIGHT_BIT_COUNT;
+    public static final int G_LIGHT_BIT_POSITION = G_INDEX * INDIVIDUAL_LIGHT_BIT_COUNT;
+    public static final int B_LIGHT_BIT_POSITION = B_INDEX * INDIVIDUAL_LIGHT_BIT_COUNT;
+    public static final int SUN_LIGHT_BIT_POSITION = SUN_INDEX * INDIVIDUAL_LIGHT_BIT_COUNT;
 
     public static final int R_LIGHT_BITS = 0xF << R_LIGHT_BIT_POSITION;
     public static final int G_LIGHT_BITS = 0xF << G_LIGHT_BIT_POSITION;
     public static final int B_LIGHT_BITS = 0xF << B_LIGHT_BIT_POSITION;
     public static final int SUN_LIGHT_BITS = 0xF << SUN_LIGHT_BIT_POSITION;
+
+    /**
+
+    /**
+     * Gets the color component of the supplied light at the specified index.
+     *
+     * @param light The light that needs its red component returned.
+     * @param index The color component's index.
+     *
+     * @return The color component of the supplied light at the specified index.
+     */
+    public static int getComponent(int light, int index) {
+        return light & (LIGHT_MAXIMUM_INTENSITY << (index * INDIVIDUAL_LIGHT_BIT_COUNT));
+    }
 
     /**
      * Gets the red component of the supplied light.
@@ -107,6 +128,19 @@ public final class VoxelLightUtils {
     }
 
     /**
+     * Gets the color component's intensity of the supplied light at the specified index. The minimum intensity is 0. The maximum intensity is defined by 2 power of {@link
+     * #INDIVIDUAL_LIGHT_BIT_COUNT}.
+     *
+     * @param light The light that needs its color component's intensity at the specified index returned.
+     * @param index The color component's index.
+     *
+     * @return The color component's intensity of the supplied light at the specified index.
+     */
+    public static int getComponentIntensity(int light, int index) {
+        return getComponent(light, index) >>> (index * INDIVIDUAL_LIGHT_BIT_COUNT);
+    }
+
+    /**
      * Gets the red component's intensity of the supplied light. The minimum intensity is 0. The maximum intensity is defined by 2 power of {@link #INDIVIDUAL_LIGHT_BIT_COUNT}.
      *
      * @param light The light that needs its red component's intensity returned.
@@ -125,7 +159,7 @@ public final class VoxelLightUtils {
      * @return The green component's intensity of the supplied light.
      */
     public static int getGreenIntensity(int light) {
-        return getGreen(light) >>> R_LIGHT_BIT_POSITION;
+        return getGreen(light) >>> G_LIGHT_BIT_POSITION;
     }
 
     /**
@@ -136,7 +170,7 @@ public final class VoxelLightUtils {
      * @return The blue component's intensity of the supplied light.
      */
     public static int getBlueIntensity(int light) {
-        return getBlue(light) >>> R_LIGHT_BIT_POSITION;
+        return getBlue(light) >>> B_LIGHT_BIT_POSITION;
     }
 
     /**
@@ -147,7 +181,7 @@ public final class VoxelLightUtils {
      * @return The sun component's intensity of the supplied light.
      */
     public static int getSunIntensity(int light) {
-        return getSun(light) >>> R_LIGHT_BIT_POSITION;
+        return getSun(light) >>> SUN_LIGHT_BIT_POSITION;
     }
 
     /**
@@ -167,7 +201,7 @@ public final class VoxelLightUtils {
      * Sets the green component's intensity of the supplied. The intensity must be inclusively between 0 and {@link #LIGHT_MAXIMUM_INTENSITY}. In other words, the intensity of a
      * light component is the value of the latter without the bit shift.
      *
-     * @param light        The light that needs its green component's intensity set.
+     * @param light          The light that needs its green component's intensity set.
      * @param greenIntensity The green component's intensity to set to the supplied light.
      *
      * @return The light which its green component's intensity was set.
@@ -180,7 +214,7 @@ public final class VoxelLightUtils {
      * Sets the blue component's intensity of the supplied. The intensity must be inclusively between 0 and {@link #LIGHT_MAXIMUM_INTENSITY}. In other words, the intensity of a
      * light component is the value of the latter without the bit shift.
      *
-     * @param light        The light that needs its blue component's intensity set.
+     * @param light         The light that needs its blue component's intensity set.
      * @param blueIntensity The blue component's intensity to set to the supplied light.
      *
      * @return The light which its blue component's intensity was set.
